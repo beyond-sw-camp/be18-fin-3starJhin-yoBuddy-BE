09f50d3aaf20123ed82acb2a6427d8d32405e685
IIIjong <pjwfish@naver.com>
2025-10-27 18:09:57 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/j3s/yobuddy/YobuddyApplication.java
@@ -0,0 +1,13 @@
+package com.j3s.yobuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j3s/yobuddy/YobuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j3s.yobuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
