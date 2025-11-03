aa3faf2b4ed6cc1262ad87288b3975b79ee33b7a
Wonseok Cho <jws020501@naver.com>
2025-11-03 15:27:13 +0900

    feat: 유저 softdelete처리 및 user 부분 예외처리 추가

--- src/main/java/com/j3s/yobuddy/domain/user/controller/UserController.java
+++ src/main/java/com/j3s/yobuddy/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,11 @@ public class UserController {
 	public ResponseEntity<Void> register(@RequestBody List<RegisterRequest> reqs) {
 		userService.register(reqs);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<Void> delete(@PathVariable Long userId) {
+		userService.softDelete(userId);
+		return ResponseEntity.noContent().build();
 	}
 }
