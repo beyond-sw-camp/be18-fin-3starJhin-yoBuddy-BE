b50c823ef301a0f4bee188c45f84c2f838fdf08c
Wonseok Cho <jws020501@naver.com>
2025-11-11 09:43:17 +0900

    refactor: 유저 리스트 조회 오류 수정완

--- src/main/java/com/j3s/yobuddy/domain/user/service/UserService.java
+++ src/main/java/com/j3s/yobuddy/domain/user/service/UserService.java
@@ -1,12 +1,14 @@
 package com.j3s.yobuddy.domain.user.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.j3s.yobuddy.domain.user.dto.RegisterRequest;
 import com.j3s.yobuddy.domain.user.dto.UpdateUserRequest;
 import com.j3s.yobuddy.domain.user.dto.UserSearchRequest;
 import com.j3s.yobuddy.domain.user.entity.User;
-import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface UserService {
 
