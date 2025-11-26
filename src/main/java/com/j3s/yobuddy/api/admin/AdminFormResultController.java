ccd6ee288308d94608c1edc6e398ac269bc2ad6d
baechuking <lsmlsm19987@naver.com>
2025-11-26 16:07:57 +0900

    feat: 폼 결과 리스트 조회 메소드 구현 (#89)

--- src/main/java/com/j3s/yobuddy/api/admin/AdminFormResultController.java
+++ src/main/java/com/j3s/yobuddy/api/admin/AdminFormResultController.java
@@ -1,13 +1,19 @@
 package com.j3s.yobuddy.api.admin;
 
 import com.j3s.yobuddy.domain.formresult.dto.request.FormResultCreateRequest;
+import com.j3s.yobuddy.domain.formresult.dto.response.FormResultListResponse;
 import com.j3s.yobuddy.domain.formresult.dto.response.FormResultResponse;
 import com.j3s.yobuddy.domain.formresult.entity.FormResult;
 import com.j3s.yobuddy.domain.formresult.service.FormResultService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,6 +43,15 @@ public class AdminFormResultController {
         formResultService.deleteFormResult(formResultId);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<FormResultListResponse>> getFormResultList(
+        @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<FormResultListResponse> formResults = formResultService.getFormResultList(pageable);
+
+        return ResponseEntity.ok(formResults);
     }
 
 }
--- src/main/java/com/j3s/yobuddy/domain/formresult/repository/FormResultRepository.java
+++ src/main/java/com/j3s/yobuddy/domain/formresult/repository/FormResultRepository.java
@@ -2,9 +2,13 @@ package com.j3s.yobuddy.domain.formresult.repository;
 
 import com.j3s.yobuddy.domain.formresult.entity.FormResult;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FormResultRepository extends JpaRepository<FormResult, Long> {
 
     Optional<FormResult> findByFormResultIdAndIsDeletedFalse(Long formResultId);
+
+    Page<FormResult> findAllByIsDeletedFalse(Pageable pageable);
 }
