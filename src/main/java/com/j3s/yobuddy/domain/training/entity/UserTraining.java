4d564c59549f246a58cb7e8abb3d42ebd6170b35
IIIjong <pjwfish@naver.com>
2025-11-26 12:33:01 +0900

    refactor: 안쓰고 코드 정리

--- src/main/java/com/j3s/yobuddy/domain/training/entity/UserTraining.java
+++ src/main/java/com/j3s/yobuddy/domain/training/entity/UserTraining.java
@@ -1,6 +1,7 @@
 package com.j3s.yobuddy.domain.training.entity;
 
 import com.j3s.yobuddy.domain.formresult.entity.FormResultStatus;
+import com.j3s.yobuddy.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,21 +11,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import com.j3s.yobuddy.domain.user.entity.User;
 
 @Entity
 @Table(name = "User_Trainings")
